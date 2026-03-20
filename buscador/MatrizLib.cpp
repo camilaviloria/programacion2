19f4d2ae8c8b559d74e4b30678bd35e6c6e6d237
CamilaViloria <camiviloria1308@gmail.com>
2026-03-19 22:19:49 -0400

    cambios

--- buscador/MatrizLib.cpp
+++
@@ -1,8 +0,0 @@
-#include "MatrizLib.hpp" 
-#include <stdexcept>     
-
-namespace MatrizLib {    
-    Matriz crearMatriz(int filas, int columnas) {
-        return Matriz(filas, columnas);
-    }
-} 
--- buscador/MatrizLib.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef MATRIZLIB_HPP
-#define MATRIZLIB_HPP
-
-#include "matriz.hpp" 
-
-namespace MatrizLib {
-    Matriz crearMatriz(int filas, int columnas);
-} 
-#endif 
