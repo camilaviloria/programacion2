c950ac5ab19c774a2c32dd3460e0bf7a11d7b256
CamilaViloria <camiviloria1308@gmail.com>
2026-05-17 20:21:34 -0400

    docs: se agrego funcion de "hello word"

--- trabajos_java/hola_mundo/src/HelloWorld.java
+++ trabajos_java/hola_mundo/src/HelloWorld.java
@@ -1,3 +1,9 @@
 public class HelloWorld {
+    public static void main(String[] args) {  //sin esta linea el codigo no funciona
+        System.out.println("Hello, World!");
+        
+    }
+
 
 }
+
