08fd021175089f88caed1be0d16bc71e22814d9d
CamilaViloria <camiviloria1308@gmail.com>
2026-05-24 20:38:02 -0400

    feat: se creo un nuevo archivo sobre variables

--- trabajos_java/hola_mundo/src/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {  //sin esta linea el codigo no funciona
-        System.out.println("Hello, World!");
-        
-    }
-
-
-}
-
---
+++ trabajos_java/hola_mundo/src/VariablesAndConstants.java
@@ -0,0 +1,14 @@
+public class VariablesAndConstants {
+    public static void main(String[] args) { 
+         String name = "camila";     
+         System.out.println(name);
+
+         name = "viloria";
+         System.out.println(name);
+         
+         int edad = 20;
+         System.out.println(edad);
+    }
+
+}
+
