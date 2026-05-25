01787841794fe58d4bef2910ead41774b56008e8
CamilaViloria <camiviloria1308@gmail.com>
2026-05-24 20:47:37 -0400

    se agregaron nuevas variables

--- trabajos_java/hola_mundo/src/VariablesAndConstants.java
+++ trabajos_java/hola_mundo/src/VariablesAndConstants.java
@@ -1,13 +1,22 @@
 public class VariablesAndConstants {
     public static void main(String[] args) { 
+        var email = "jdhbcwhdbciwe";
+        System.out.println(email);
+
+        var fecha = 2026;
+        System.out.println(fecha);
+
          String name = "camila";     
          System.out.println(name);
 
          name = "viloria";
          System.out.println(name);
-         
+
          int edad = 20;
          System.out.println(edad);
+
+         final String EMAIL = "camiviloria12@gmail.com"; //constante no se puede actu (MAYUSCULAS)
+         System.out.println(EMAIL);
     }
 
 }
