4808579021df88de1d27e2b3a624ae02834a3b01
CamilaViloria <camiviloria1308@gmail.com>
2026-05-24 21:44:53 -0400

    feat: se agregaron mas ejercicios de java

---
+++ trabajos_java/hola_mundo/src/operadores.java
@@ -0,0 +1,33 @@
+public class operadores {
+    public static void main(String[] args) {
+        //aritmeticos
+        var a = 4;
+        var b = 9;
+    
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        //asignacion
+        a = b;
+        System.err.println(a);
+
+        a = b * 2;
+        System.err.println(a);
+
+        a += 1;
+        System.err.println(a);
+
+        a -= 1;
+        System.err.println(a);
+        a *= 1;
+        System.err.println(a);
+        a /= 1;
+        System.err.println(a);
+
+
+
+    }
+}
