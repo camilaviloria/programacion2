e151b65b0c6700f47e047cf5d518a979676a9766
CamilaViloria <camiviloria1308@gmail.com>
2026-06-02 21:35:41 -0400

    se agregaron nuevos ejercicios

--- trabajos_java/hola_mundo/src/condicionales.java
+++ trabajos_java/hola_mundo/src/condicionales.java
@@ -1,11 +1,31 @@
 public class condicionales {
     public static void main(String[] args) {
-        // condicionales
-        var edad = 20;
-        if (edad >= 18) {
+        // condicionales (secuencias)
+        var edad = 18;
+        if (edad > 18) {
             System.out.println("es mayor de edad");
+        } else if (edad == 18) {
+            System.out.println("tiene 18 años");
         } else {
             System.out.println("es menor de edad");
+        } 
+
+        //switch
+
+        var dia = 3;
+        switch (dia) {
+            case 1:
+                System.out.println("lunes");
+                break;
+            case 2:
+                System.out.println("martes");
+                break;
+            case 3:
+                System.out.println("miercoles");
+                break;
+            default:
+                System.out.println("dia no valido");
+                break;
         }
 
     }
