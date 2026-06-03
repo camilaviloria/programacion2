25cef3522698d3b76284e0ba3b9bc4e0a483e9d6
CamilaViloria <camiviloria1308@gmail.com>
2026-06-03 14:00:11 -0400

    feat: nuevos ejercicios

--- trabajos_java/hola_mundo/src/arrays.java
+++ trabajos_java/hola_mundo/src/arrays.java
@@ -4,7 +4,7 @@ public class arrays {
         int[] numbers = new int[3];
         System.out.println(numbers.length);
         String[] names = {"camila", "viloria", "camila"};
-        System.out.println(names.length);
+        System.out.println(names);
 
         //acceso
         System.out.println(names[1]);
@@ -13,5 +13,14 @@ public class arrays {
         System.out.println((new String[3])[0]);
 
         //modificacion
+        System.out.println(numbers[0]);
+        numbers[0] = 10;
+        System.out.println(numbers[0]);
+
+        names[0] = null; // null es compatible con las cadenas de textos
+        System.out.println(names[0]);
+
+        
+
     }
 }
