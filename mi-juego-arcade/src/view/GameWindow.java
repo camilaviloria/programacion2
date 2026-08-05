66c9036ff07c39ad55456fcca2045cff3e57823a
CamilaViloria <camiviloria1308@gmail.com>
2026-08-05 10:14:27 -0400

    feat: se agrego ScoreBoardPanel y GameWindow

--- mi-juego-arcade/src/view/GameWindow.java
+++ mi-juego-arcade/src/view/GameWindow.java
@@ -1,5 +1,34 @@
-package mi-juego-arcade.src.view;
+package view;
 
-public class GameWindow {
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import model.GameModel;
 
+public class GameWindow extends JFrame {
+    private final GamePanel gamePanel;
+    private final ScoreBoardPanel scoreBoardPanel;
+
+    public GameWindow(GameModel model) {
+        setTitle("Snake Arcade");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(new BorderLayout());
+
+        gamePanel = new GamePanel(model);
+        scoreBoardPanel = new ScoreBoardPanel();
+
+        add(gamePanel, BorderLayout.CENTER);
+        add(scoreBoardPanel, BorderLayout.EAST);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public GamePanel getGamePanel() {
+        return gamePanel;
+    }
+
+    public void updateScoreBoard() {
+        scoreBoardPanel.refreshScores();
+    }
 }
--- mi-juego-arcade/src/view/ScoreBoardPanel.java
+++ mi-juego-arcade/src/view/ScoreBoardPanel.java
@@ -1,5 +1,53 @@
-package mi-juego-arcade.src.view;
+package view;
 
-public class ScoreBoardPanel {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.List;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import model.PlayerScore;
+import model.ScoreManager;
 
+public class ScoreBoardPanel extends JPanel {
+    private final JTable scoreTable;
+    private final DefaultTableModel tableModel;
+
+    public ScoreBoardPanel() {
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(220, 400));
+
+        JLabel titleLabel = new JLabel("Tabla de Posiciones", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        add(titleLabel, BorderLayout.NORTH);
+
+        String[] columnNames = {"Jugador", "Puntos"};
+        tableModel = new DefaultTableModel(columnNames, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false; 
+            }
+        };
+
+        scoreTable = new JTable(tableModel);
+        scoreTable.setFillsViewportHeight(true);
+
+        JScrollPane scrollPane = new JScrollPane(scoreTable);
+        add(scrollPane, BorderLayout.CENTER);
+
+        refreshScores();
+    }
+
+    public void refreshScores() {
+        tableModel.setRowCount(0); 
+        List<PlayerScore> scores = ScoreManager.loadScores();
+
+        for (PlayerScore ps : scores) {
+            tableModel.addRow(new Object[]{ps.getName(), ps.getScore()});
+        }
+    }
 }
